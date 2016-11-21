ac861860624081cda43210112278e738afc31733
Leo9073 <Leo@leomatomacbook-pro.local>
2016-11-21 15:34:49 +0800

    Initial Commit

---
+++ LLCarsTableViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LLCarsTableViewDemo
+//
+//  Created by Leo on 11/21/16.
+//  Copyright © 2016 Leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
